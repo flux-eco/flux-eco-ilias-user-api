9da28ba4bc2fd12d20726b9370615efe82d1d313
Martin Studer, fluxlabs ag <martin@fluxlabs.ch>
2023-02-01 15:34:05 +0100

    updated rest client

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sh ../app/bin/install-libraries.sh
-docker build  ../ -f Dockerfile --target flux-eco-ilias-user-orbital -t fluxms/flux-eco-ilias-user-orbital:v2022-01-08-7
+docker build  ../ -f Dockerfile --target flux-eco-ilias-user-orbital -t fluxms/flux-eco-ilias-user-orbital:v2023-02-01-1
